5889a321ca60a8a8feb0f1d9d1c416e51e43227c
Đức <duc894580@gmail.com>
2024-04-22 13:21:08 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="app">
+    <img src="" alt="">
+  </div>
+</template>
+
+<style scoped lang="scss">
+* {
+  box-sizing: inherit;
+}
+
+html {
+  font-size: 62.5%;
+  line-height: 1.6rem;
+  font-family: 'Roboto', sans-serif;
+  box-sizing: border-box;
+}
+
+#app {
+  max-width: 376px;
+  margin: 0 auto;
+  padding: 0 16px;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #fff;
+}
+</style>
