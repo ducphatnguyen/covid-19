1e2cb18d98869d48cd17d72fa6b35310406a1feb
Đức <duc894580@gmail.com>
2024-05-13 13:50:12 +0700

    ♻️  [all pages] refactor to clean code

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `@import "src/styles/_variables.scss";`,
-      }
-    }
+      },
+    },
   },
 
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   server: {
     port: 8080,
   },
-})
+});
